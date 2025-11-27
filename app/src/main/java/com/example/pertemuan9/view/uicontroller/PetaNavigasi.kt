c32348ad46a162371835e65ff2bd536b4d97851c
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-27 13:20:09 +0700

    konfigurasi PetaNavigasi.kt

---
+++ app/src/main/java/com/example/pertemuan9/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,40 @@
+package com.example.pertemuan9.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan9.view.route.DestinasiHome
+import com.example.pertemuan9.view.route.DestinasiEntry
+import com.example.pertemuan9.view.EntrySiswaScreen
+import com.example.pertemuan9.view.HomeScreen
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier = Modifier) {
+    HostNavigasi(navController = navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        composable(route = DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(route = DestinasiEntry.route) },
+            )
+        }
+        composable(route = DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
