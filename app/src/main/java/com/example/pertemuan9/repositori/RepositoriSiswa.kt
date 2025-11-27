607032b4aeae8fc6878c4fd8e2cf60790907fc87
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-27 09:33:09 +0700

    mmebuat file RepositoriSiswa.kt/ membuat fun getallsiswastream/ suspend fun insertsiswa/ class offlinerepositorisiswa/override fun getallsiswastream/ override suspend fun/ minor fixes

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -1,6 +1,8 @@
 package com.example.pertemuan9.repositori
 
+import com.example.pertemuan9.room.Siswa
 import com.example.pertemuan9.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
