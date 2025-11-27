90575d2d398cbce18fc0a6034a21c4e6e03709bd
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-27 13:34:24 +0700

    membuat HomeViewModel.kt

---
+++ app/src/main/java/com/example/pertemuan9/viewmodel/HomeViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.pertemuan9.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan9.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import com.example.pertemuan9.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa) : ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    val homeUiState: StateFlow<HomeUiState> =
+        repositoriSiswa.getAllSiswaStream()
+            .filterNotNull()
+            .map {
+                HomeUiState(listSiswa = it.toList())
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(stopTimeoutMillis = TIMEOUT_MILLIS),
+                initialValue = HomeUiState()
+            )
+}
+
+data class HomeUiState(
+    val listSiswa: List<Siswa> = listOf()
+)
