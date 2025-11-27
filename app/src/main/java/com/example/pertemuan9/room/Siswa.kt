0179654c194e05fd06176559cd46f6e8b4da1709
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-27 09:15:24 +0700

    konfigurasi pertama

---
+++ app/src/main/java/com/example/pertemuan9/room/Siswa.kt
@@ -0,0 +1,15 @@
+package com.example.pertemuan9.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama: String,
+    val alamat: String,
+    val telpon: String
+)
+
+
