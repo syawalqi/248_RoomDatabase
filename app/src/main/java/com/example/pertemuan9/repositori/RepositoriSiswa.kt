6590ed3b92b6ab24e848d651b2b5d91b4fb051af
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-11-27 09:30:44 +0700

    mmebuat file RepositoriSiswa.kt/ membuat fun getallsiswastream/ suspend fun insertsiswa/ class offlinerepositorisiswa

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -4,8 +4,7 @@ interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
 
     suspend fun insertSiswa(siswa: Siswa)
-
-
-
 }
 
+class OfflineRepositoriSiswa(private val siswaDao: SiswaDao
+) : RepositoriSiswa {
