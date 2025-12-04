89de5bbe2ab023416bb01f7d72598278a999915b
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-04 08:48:52 +0700

    membuat DestinasiDetailSiswa.kt dan konfigurasi

---
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan9.view.route
+
+import com.example.pertemuan9.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "${route}/{${itemIdArg}}"
+}
