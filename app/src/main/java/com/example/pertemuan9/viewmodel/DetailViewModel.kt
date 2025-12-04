ef2096633c0899e74634bdaf93a649e654daee57
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-04 08:36:01 +0700

    membuat DetailViewModel.kt dan juga konfigurasi

--- app/src/main/java/com/example/pertemuan9/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/DetailViewModel.kt
@@ -1,4 +1,48 @@
 package com.example.pertemuan9.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan9.repositori.RepositoriSiswa
+import com.example.pertemuan9.view.route.DestinasiDetailSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
 
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetailSiswa.itemIdArg])
+
+    val uiDetailState: StateFlow<DetailSiswaUiState> =
+        repositoriSiswa.getSiswaStream(idSiswa)
+            .filterNotNull()
+            .map {
+                DetailSiswaUiState(detailSiswa = it.toDetailSiswa())
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = DetailSiswaUiState()
+            )
+
+    suspend fun deleteSiswa() {
+        repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
+    }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+/**
+ * UI state untuk DetailSiswaScreen
+ */
+data class DetailSiswaUiState(
+    val detailSiswa: DetailSiswa = DetailSiswa()
+)
