5e9da6187b76c55392efc6086027ee968e1907dd
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-04 08:17:43 +0700

    mengubah RepositoriSiswa.kt getsiswastream

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
-
     suspend fun insertSiswa(siswa: Siswa)
 
     fun getSiswaStream(id: Int): Flow<Siswa?>
---
+++ app/src/main/java/com/example/pertemuan9/viewmodel/DetailViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.pertemuan9.viewmodel
+
+
+
