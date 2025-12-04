d4c8cf2dbeddef56c21da811b43cb3937e07b3a1
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-04 08:00:55 +0700

    mengubah SiswaDao.kt select id siswa

--- app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -14,4 +14,7 @@ interface SiswaDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
 }
