35c520fc0883f0399454e753e93aa9c821a4a16b
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-04 09:41:26 +0700

    fixing DestinasiDetailSiswa.kt

--- app/src/main/java/com/example/pertemuan9/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/view/route/DestinasiDetailSiswa.kt
@@ -5,6 +5,8 @@ import com.example.pertemuan9.R
 object DestinasiDetailSiswa : DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
+
     const val itemIdArg = "idSiswa"
-    val routeWithArgs = "${route}/{${itemIdArg}}"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
+
