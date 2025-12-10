d37cf545d01754616602592f154517d610c21cce
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-10 19:36:15 +0700

    membuat add update ke SiswaDao.kt

--- app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -8,16 +8,16 @@ import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
-
 @Dao
 interface SiswaDao {
-    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+
+    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
 
-    @Query("SELECT * from tblSiswa WHERE id = :id")
+    @Query("SELECT * FROM tblSiswa WHERE id = :id")
     fun getSiswa(id: Int): Flow<Siswa?>
 
     @Update
