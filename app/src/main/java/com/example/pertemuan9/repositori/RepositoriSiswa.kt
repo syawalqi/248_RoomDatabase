9f71ff8c2e9d99a7f7ecdf6d724583b1d23bb368
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-12-10 19:35:48 +0700

    membuat add update ke RepositoriSiswa.kt

--- app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/RepositoriSiswa.kt
@@ -8,19 +8,26 @@ interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
     fun getSiswaStream(id: Int): Flow<Siswa?>
-
     suspend fun deleteSiswa(siswa: Siswa)
-
     suspend fun updateSiswa(siswa: Siswa)
 }
 
-class OfflineRepositoriSiswa(private val siswaDao: SiswaDao
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
 ) : RepositoriSiswa {
-    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
-    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
-    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
-    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 
+    override fun getAllSiswaStream(): Flow<List<Siswa>> =
+        siswaDao.getAllSiswa()
 
+    override suspend fun insertSiswa(siswa: Siswa) =
+        siswaDao.insert(siswa)
+
+    override fun getSiswaStream(id: Int): Flow<Siswa?> =
+        siswaDao.getSiswa(id)
+
+    override suspend fun deleteSiswa(siswa: Siswa) =
+        siswaDao.delete(siswa)
+
+    override suspend fun updateSiswa(siswa: Siswa) =
+        siswaDao.update(siswa)
 }
